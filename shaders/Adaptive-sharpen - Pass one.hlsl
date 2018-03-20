46c0005035b2494129dbec3444a15bcda43c5dea
bacondither <ekholm.john@gmail.com>
2018-03-20 15:12:43 +0100

    Bump copyright year to 2018

--- shaders/Adaptive-sharpen - Pass one.hlsl
+++ shaders/Adaptive-sharpen - Pass one.hlsl
@@ -1,6 +1,6 @@
 // $MinimumShaderProfile: ps_3_0
 
-// Copyright (c) 2015-2017, bacondither
+// Copyright (c) 2015-2018, bacondither
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
